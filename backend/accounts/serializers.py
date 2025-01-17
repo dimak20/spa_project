55171325011b6227950b7d06d63bea4df4d12e3b
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-17 12:25:44 +0200

    Add user serializers

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", "email", "first_name", "last_name", "password", "is_staff"]
+        read_only_fields = [
+            "id",
+            "is_staff",
+        ]
+        extra_kwargs = {
+            "password": {
+                "write_only": True,
+                "min_length": 5,
+                "style": {"input_type": "password"},
+            }
+        }
+
+    def create(self, validated_data):
+        """create user with encrypted password"""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class UserRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            "id",
+            "username",
+            "email",
+            "profile_image"
+        ]
