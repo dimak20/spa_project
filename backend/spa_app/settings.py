65bf97a7ebd9e3f8cece0922fae297b326d5ef69
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-17 12:32:45 +0200

    Add rest default setting to settings.py

--- backend/spa_app/settings.py
+++ backend/spa_app/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,9 +40,9 @@ INSTALLED_APPS = [
     # 3rd party
     "rest_framework",
     "rest_framework_simplejwt",
-    #my_apps
+    # my_apps
     "comments",
-    "accounts"
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +129,9 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
