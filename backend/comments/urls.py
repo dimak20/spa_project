937697024025e780e5baac14c70b7e03842bb391
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-17 14:53:55 +0200

    Add comment router to urls.py

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from comments.views import CommentView
+
 app_name = "comments"
-
+router = routers.DefaultRouter()
+router.register("comments", CommentView)
 urlpatterns = [
-
+    path("", include(router.urls))
 ]
