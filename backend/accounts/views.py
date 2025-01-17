4eea0e01cf03d5ae727fb64b4130512862d5c33b
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-17 12:27:16 +0200

    Add create user view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from accounts.serializers import (
+    UserSerializer
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = ()
