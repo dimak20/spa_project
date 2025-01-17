98e44cc55dc9d80d74de136ae6ed8bcf705feaf8
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-17 14:12:03 +0200

    Add empty comment view

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework import generics
 
-# Create your views here.
+from comments.filters import CommentFilter
+from comments.models import Comment
+
+
+class CommentView(generics.ListCreateAPIView):
+    model = Comment
+    queryset = Comment.objects.all()
+    filterset_class = CommentFilter
+    filter_backends = (DjangoFilterBackend,)
+    ordering_fields = [
+        "created_at",
+        "user__username",
+        "user__email"
+    ]
