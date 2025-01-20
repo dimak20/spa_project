3010417b0a55e58a97158e4d889e4074f2fd9d82
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-20 16:11:25 +0200

    Add recaptcha component

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(
+            {
+                template: {
+                    compilerOptions: {
+                        isCustomElement: (tag) => tag === 'VueRecaptcha',
+                    }
+                }
+            }
+        ),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
