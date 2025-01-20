83f17548e648ff74ae47b901dd174050f795b5a8
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-20 16:09:05 +0200

    Add captcha verification to comment serializer

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -4,6 +4,7 @@ from comments.models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    captcha = serializers.CharField(write_only=True)
     class Meta:
         model = Comment
         fields = [
@@ -15,15 +16,21 @@ class CommentSerializer(serializers.ModelSerializer):
             "created_at",
             "attached_image",
             "attached_file",
-            "parent"
+            "parent",
+            "captcha"
         ]
         read_only_fields = [
             "user",
             "id",
             "created_at",
             "updated_at",
-            "parent"
         ]
+
+    def validate(self, data):
+        captcha_response = data.pop("captcha", None)
+        if not captcha_response:
+            raise serializers.ValidationError({"captcha": "Captcha is required."})
+        return data
 
 
 class CommentListSerializer(CommentSerializer):
@@ -41,21 +48,18 @@ class CommentListSerializer(CommentSerializer):
             "created_at",
             "attached_image",
             "attached_file",
-            "replies"
+            "replies",
         ]
-        read_only_fields = [
-            "user",
-            "id",
-            "created_at",
-            "updated_at",
-            "parent"
-        ]
+        read_only_fields = ["user", "id", "created_at", "updated_at", "parent"]
 
     def get_user(self, obj: Comment):
         return {
             "id": obj.user.id,
             "username": obj.user.username,
-            "email": obj.user.email
+            "email": obj.user.email,
+            "profile_image": (
+                obj.user.profile_image.url if obj.user.profile_image else None
+            ),
         }
 
     def get_replies(self, obj: Comment):
