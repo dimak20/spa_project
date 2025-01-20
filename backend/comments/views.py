cc114e12a80cc9881fa263af4a8f2b67cec2b989
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-20 16:06:59 +0200

    Add captcha verification to comment viewset

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,27 +1,25 @@
 from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import viewsets, mixins
+from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter
 
 from comments.filters import CommentFilter
 from comments.models import Comment
 from comments.paginators import CustomCommentPagination
 from comments.serializers import CommentSerializer, CommentListSerializer
+from comments.utils import verify_recaptcha
 
 
 class CommentView(
     viewsets.GenericViewSet,
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
-    mixins.UpdateModelMixin
+    mixins.UpdateModelMixin,
 ):
-    queryset = Comment.objects.all()
+    queryset = Comment.objects.filter(parent=None)
     filterset_class = CommentFilter
     filter_backends = (DjangoFilterBackend, OrderingFilter)
-    ordering_fields = [
-        "created_at",
-        "user__username",
-        "user__email"
-    ]
+    ordering_fields = ["created_at", "user__username", "user__email"]
     ordering = ["-created_at"]
     pagination_class = CustomCommentPagination
 
@@ -32,15 +30,16 @@ class CommentView(
         return CommentSerializer
 
     def perform_create(self, serializer):
+        recaptcha_token = self.request.data.get("captcha")
+        result = verify_recaptcha(recaptcha_token)
+        if not result.get("success"):
+            raise ValidationError("Invalid reCAPTCHA.")
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
         queryset = super().get_queryset()
 
         if self.action == "list":
-            return queryset.prefetch_related(
-                "user",
-                "replies__user"
-            )
+            return queryset.prefetch_related("user", "replies__user")
 
         return queryset
