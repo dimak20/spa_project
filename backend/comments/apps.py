f91a001ee442963a952a8bfe57840a7943181224
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-21 18:58:00 +0200

    Register signals in comment app

--- backend/comments/apps.py
+++ backend/comments/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
-class CommentsConfig(AppConfig):
+class YourAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "comments"
+
+    def ready(self):
+        import comments.signals
