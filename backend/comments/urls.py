bbe5ad0d6c8ba2340cac61002cccb8e12f6dc657
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-21 17:44:19 +0200

    Fix lines in urls.py

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 from comments.views import CommentView
 
 app_name = "comments"
+
 router = routers.DefaultRouter()
 router.register("", CommentView, basename="comment")
 urlpatterns = [
