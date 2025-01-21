dd29db520c72e3df2507361bf30c2630f9c5bf3a
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-21 18:58:40 +0200

    Add cache decorator to list method in Comment view set

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -2,12 +2,13 @@ from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework import viewsets, mixins
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import OrderingFilter
+from rest_framework_extensions.cache.decorators import cache_response
 
 from comments.filters import CommentFilter
 from comments.models import Comment
 from comments.paginators import CustomCommentPagination
 from comments.serializers import CommentSerializer, CommentListSerializer
-from comments.utils import verify_recaptcha
+from comments.utils import verify_recaptcha, generate_key
 
 
 class CommentView(
@@ -28,6 +29,10 @@ class CommentView(
             return CommentListSerializer
 
         return CommentSerializer
+
+    @cache_response(60 * 3, key_func=generate_key)
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         recaptcha_token = self.request.data.get("captcha")
