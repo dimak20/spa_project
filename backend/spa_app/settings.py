ddea6d0cd54a8fc469ebee4135e44c281280f7ae
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-22 12:50:21 +0200

    Add debug mode to env

--- backend/spa_app/settings.py
+++ backend/spa_app/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "host.docker.internal"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "host.docker.internal", "spa-project-1.onrender.com"]
 
 # Application definition
 
