42bd6feea4c4cea8a57dfd9a04a2316e942c7605
Dmytro Kolchak <dima.kolhac@gmail.com>
2025-01-22 13:34:28 +0200

    Add storages to requirements.txt

--- backend/spa_app/settings.py
+++ backend/spa_app/settings.py
@@ -24,7 +24,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
